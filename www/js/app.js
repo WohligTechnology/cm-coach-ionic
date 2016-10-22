4a38b1373023e2e3988235492ed430089d6307fe
TusharSachde <tushar@wohlig.com>
2016-10-22 13:28:51 +0530

    Auto Grow

--- www/js/app.js
+++ www/js/app.js
@@ -329,4 +329,54 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   };
 })
 
-;
+.directive('autoGrow', function ($window) {
+  return {
+    restrict: 'A',
+    link: function ($scope, $element, $attrs) {
+      $scope.attrs = {
+        rows: 1,
+        maxLines: 999
+      };
+      for (var i in $scope.attrs) {
+        if ($attrs[i]) {
+          $scope.attrs[i] = parseInt($attrs[i]);
+        }
+      }
+      $scope.getOffset = function () {
+        var style = $window.getComputedStyle($element[0], null),
+          props = ['paddingTop', 'paddingBottom'],
+          offset = 0;
+
+        for (var i = 0; i < props.length; i++) {
+          offset += parseInt(style[props[i]]);
+        }
+        return offset;
+      };
+      $scope.autogrowFn = function () {
+        var newHeight = 0,
+          hasGrown = false;
+        if (($element[0].scrollHeight - $scope.offset) > $scope.maxAllowedHeight) {
+          $element[0].style.overflowY = 'scroll';
+          newHeight = $scope.maxAllowedHeight;
+        } else {
+          $element[0].style.overflowY = 'hidden';
+          $element[0].style.height = 'auto';
+          newHeight = $element[0].scrollHeight - $scope.offset;
+          hasGrown = true;
+        }
+        $element[0].style.height = newHeight + 'px';
+        return hasGrown;
+      };
+
+      $scope.offset = $scope.getOffset();
+      $scope.lineHeight = ($element[0].scrollHeight / $scope.attrs.rows) - ($scope.offset / $scope.attrs.rows);
+      $scope.maxAllowedHeight = ($scope.lineHeight * $scope.attrs.maxLines) - $scope.offset;
+
+      $element[0].addEventListener('input', $scope.autogrowFn);
+      if ($element[0].value !== '') {
+        $scope.autogrowFn();
+      }
+    }
+
+  };
+});
